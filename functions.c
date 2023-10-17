9856f9560d3331c97e0de63b9c3ecb9a945437c1
Mihret001 <Bizumercy@gmail.com>
2023-10-17 13:59:25 -0700

    edited functions.c

--- functions.c
+++ functions.c
@@ -6,74 +6,73 @@
  * @argList: List of arguments.
  * @outputBuffer: Buffer array to handle print.
  * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
+ * @width: Width of the output field.
  * @precision: Precision specification.
- * @dataSize: Size specifier.
+ * @size: Size specifier.
  * Return: Number of characters printed.
  */
+
 int printFormattedCharacter(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
+		int activeFlags, int width, int precision, int size)
 {
-    char ch = va_arg(argList, int);
+	char ch = va_arg(argList, int);
 
-    return (handleWriteCharacter(ch, outputBuffer, activeFlags, fieldWidth, precision, dataSize));
+	return (handle_write_char(ch, outputBuffer, activeFlags,
+				width, precision, size));
 }
-
 /************************* PRINT STRING *************************/
 /**
  * printFormattedString - Prints a string
  * @argList: List of arguments.
  * @outputBuffer: Buffer array to handle print.
  * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
+ * @width: Width of the output field.
  * @precision: Precision specification.
- * @dataSize: Size specifier.
+ * @size: Size specifier.
  * Return: Number of characters printed.
  */
+
 int printFormattedString(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
+		int activeFlags, int width, int precision, int size)
 {
-    int length = 0, x;
-    char *str = va_arg(argList, char *);
+	int len = 0, x;
+	char *s = va_arg(argList, char *);
 
-    UNUSED(outputBuffer);
-    UNUSED(activeFlags);
-    UNUSED(fieldWidth);
-    UNUSED(precision);
-    UNUSED(dataSize);
+	UNUSED(outputBuffer);
+	UNUSED(activeFlags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-    if (str == NULL)
-    {
-        str = "(null)";
-        if (precision >= 6)
-            str = "      ";
-    }
+	if (s == NULL)
+	{
+		s = "(null)";
+		if (precision >= 6)
+			s = "      ";
+	}
 
-    while (str[length] != '\0')
-        length++;
-
-    if (precision >= 0 && precision < length)
-        length = precision;
-
-    if (fieldWidth > length)
-    {
-        if (activeFlags & F_MINUS)
-        {
-            write(1, &str[0], length);
-            for (x = fieldWidth - length; x > 0; x--)
-                write(1, " ", 1);
-            return fieldWidth;
-        }
-        else
-        {
-            for (x = fieldWidth - length; x > 0; x--)
-                write(1, " ", 1);
-            write(1, &str[0], length);
-            return fieldWidth;
-        }
-    }
-
-    return write(1, str, length);
+	while (s[len] != '\0')
+		len++;
+	if (precision >= 0 && precision < len)
+		len = precision;
+	if (width > len)
+	{
+		if (activeFlags & F_MINUS)
+		{
+			write(1, &s[0], len);
+			for (x = width - len; x > 0; x--)
+			write(1, " ", 1);
+			return (width);
+		}
+		else
+		{
+			for (x = width - len; x > 0; x--)
+				write(1, " ", 1);
+			write(1, &s[0], len);
+			return (width);
+		}
+	}
+	return (write(1, s, len));
 }
 
 /************************* PRINT PERCENT SIGN *************************/
@@ -82,22 +81,23 @@ int printFormattedString(va_list argList, char outputBuffer[],
  * @argList: List of arguments.
  * @outputBuffer: Buffer array to handle print.
  * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
+ * @width: Width of the output field.
  * @precision: Precision specification.
- * @dataSize: Size specifier.
+ * @size: Size specifier.
  * Return: Number of characters printed.
  */
-int printFormattedPercentSign(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
-{
-    UNUSED(argList);
-    UNUSED(outputBuffer);
-    UNUSED(activeFlags);
-    UNUSED(fieldWidth);
-    UNUSED(precision);
-    UNUSED(dataSize);
 
-    return write(1, "%%", 1);
+int printFormattedPercentSign(va_list argList, char outputBuffer[],
+		int activeFlags, int width, int precision, int size)
+{
+	UNUSED(argList);
+	UNUSED(outputBuffer);
+	UNUSED(activeFlags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
+
+	return (write(1, "%%", 1));
 }
 
 /************************* PRINT INTEGER *************************/
@@ -106,196 +106,44 @@ int printFormattedPercentSign(va_list argList, char outputBuffer[],
  * @argList: List of arguments.
  * @outputBuffer: Buffer array to handle print.
  * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
+ * @width: Width of the output field.
  * @precision: Precision specification.
- * @dataSize: Size specifier.
+ * @size: Size specifier.
  * Return: Number of characters printed.
  */
+
 int printFormattedInteger(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
+		int activeFlags, int width, int precision, int size)
 {
-    int x = MAX_BUFFER_SIZE - 2;
-    int isNegative = 0;
-    long int number = va_arg(argList, long int);
-    unsigned long int num;
+	int x = BUFF_SIZE - 2;
+		int neg = 0;
+		long int no = va_arg(argList, long int);
+		unsigned long int number;
 
-    number = convertSizeNumber(number, dataSize);
+		no = convert_size_number(no, size);
 
-    if (number == 0)
-        outputBuffer[x--] = '0';
+		if (no == 0)
+			outputBuffer[x--] = '0';
 
-    outputBuffer[MAX_BUFFER_SIZE - 1] = '\0';
-    num = (unsigned long int)number;
+		outputBuffer[BUFFER_SIZE - 1] = '\0';
+		number = (unsigned long int)no;
 
-    if (number < 0)
-    {
-        num = (unsigned long int)((-1) * number);
-        isNegative = 1;
-    }
+		if (no < 0)
+		{
+			number = (unsigned long int)((-1) * no);
+			neg = 1;
+		}
 
-    while (num > 0)
-    {
-        outputBuffer[x--] = (num % 10) + '0';
-        num /= 10;
-    }
+		while (number > 0)
+		{
+			outputBuffer[x--] = (number % 10) + '0';
+			number /= 10;
+		}
 
-    x++;
+		x++;
 
-    return writeNumber(isNegative, x, outputBuffer, activeFlags, fieldWidth, precision, dataSize);
-}
-
-/************************* PRINT BINARY *************************/
-/**
- * printFormattedBinary - Prints an unsigned number in binary format
- * @argList: List of arguments.
- * @outputBuffer: Buffer array to handle print.
- * @activeFlags: Flags indicatingthe formattingHere is the modified code with the variable `i` changed to `x`:
-
-```c
-#include "main.h"
-
-/************************* PRINT CHARACTER *************************/
-/**
- * printFormattedCharacter - Prints a character
- * @argList: List of arguments.
- * @outputBuffer: Buffer array to handle print.
- * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
- * @precision: Precision specification.
- * @dataSize: Size specifier.
- * Return: Number of characters printed.
- */
-int printFormattedCharacter(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
-{
-    char ch = va_arg(argList, int);
-
-    return (handleWriteCharacter(ch, outputBuffer, activeFlags, fieldWidth, precision, dataSize));
-}
-
-/************************* PRINT STRING *************************/
-/**
- * printFormattedString - Prints a string
- * @argList: List of arguments.
- * @outputBuffer: Buffer array to handle print.
- * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
- * @precision: Precision specification.
- * @dataSize: Size specifier.
- * Return: Number of characters printed.
- */
-int printFormattedString(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
-{
-    int length = 0, x;
-    char *str = va_arg(argList, char *);
-
-    UNUSED(outputBuffer);
-    UNUSED(activeFlags);
-    UNUSED(fieldWidth);
-    UNUSED(precision);
-    UNUSED(dataSize);
-
-    if (str == NULL)
-    {
-        str = "(null)";
-        if (precision >= 6)
-            str = "      ";
-    }
-
-    while (str[length] != '\0')
-        length++;
-
-    if (precision >= 0 && precision < length)
-        length = precision;
-
-    if (fieldWidth > length)
-    {
-        if (activeFlags & F_MINUS)
-        {
-            write(1, &str[0], length);
-            for (x = fieldWidth - length; x > 0; x--)
-                write(1, " ", 1);
-            return fieldWidth;
-        }
-        else
-        {
-            for (x = fieldWidth - length; x > 0; x--)
-                write(1, " ", 1);
-            write(1, &str[0], length);
-            return fieldWidth;
-        }
-    }
-
-    return write(1, str, length);
-}
-
-/************************* PRINT PERCENT SIGN *************************/
-/**
- * printFormattedPercentSign - Prints a percent sign
- * @argList: List of arguments.
- * @outputBuffer: Buffer array to handle print.
- * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
- * @precision: Precision specification.
- * @dataSize: Size specifier.
- * Return: Number of characters printed.
- */
-int printFormattedPercentSign(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
-{
-    UNUSED(argList);
-    UNUSED(outputBuffer);
-    UNUSED(activeFlags);
-    UNUSED(fieldWidth);
-    UNUSED(precision);
-    UNUSED(dataSize);
-
-    return write(1, "%%", 1);
-}
-
-/************************* PRINT INTEGER *************************/
-/**
- * printFormattedInteger - Prints an integer
- * @argList: List of arguments.
- * @outputBuffer: Buffer array to handle print.
- * @activeFlags: Flags indicating the formatting options.
- * @fieldWidth: Width of the output field.
- * @precision: Precision specification.
- * @dataSize: Size specifier.
- * Return: Number of characters printed.
- */
-int printFormattedInteger(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
-{
-    int x = MAX_BUFFER_SIZE - 2;
-    int isNegative = 0;
-    long int number = va_arg(argList, long int);
-    unsigned long int num;
-
-    number = convertSizeNumber(number, dataSize);
-
-    if (number == 0)
-        outputBuffer[x--] = '0';
-
-    outputBuffer[MAX_BUFFER_SIZE - 1] = '\0';
-    num = (unsigned long int)number;
-
-    if (number < 0)
-    {
-        num = (unsigned long int)((-1) * number);
-        isNegative = 1;
-    }
-
-    while (num > 0)
-    {
-        outputBuffer[x--] = (num % 10) + '0';
-        num /= 10;
-    }
-
-    x++;
-
-    return writeNumber(isNegative, x, outputBuffer, activeFlags, fieldWidth, precision, dataSize);
+		return (write_number(neg, x, outputBuffer, activeFlags,
+				width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
@@ -304,31 +152,42 @@ int printFormattedInteger(va_list argList, char outputBuffer[],
  * @argList: List of arguments.
  * @outputBuffer: Buffer array to handle print.
  * @activeFlags: Flags indicatingthe formatting options.
- * * @fieldWidth: Width of the output field.
+ * @width: Width of the output field.
  * @precision: Precision specification.
- * @dataSize: Size specifier.
+ * @size: Size specifier.
  * Return: Number of characters printed.
  */
 int printFormattedBinary(va_list argList, char outputBuffer[],
-    int activeFlags, int fieldWidth, int precision, int dataSize)
+		int activeFlags, int width, int precision, int size)
 {
-    unsigned int number = va_arg(argList, unsigned int);
-    int x = MAX_BUFFER_SIZE - 2;
+	unsigned int a, b, c, ans;
+	unsigned int d[32];
+	int num;
 
-    number = convertSizeNumber(number, dataSize);
+	UNUSED(outputBuffer);
+	UNUSED(activeFlags);
+	UNUSED(width);
+	UNUSED(precision);
+	UNUSED(size);
 
-    if (number == 0)
-        outputBuffer[x--] = '0';
+	a = va_arg(argList, unsigned int);
+	b = 2147483648;
+	d[0] = a / b;
 
-    outputBuffer[MAX_BUFFER_SIZE - 1] = '\0';
+	for (c = 1; c < 32; c++)
+	{
+		b /= 2;
+		d[c] = (a / b) % 2;
+	}
+	for (c = 0, ans = 0, num = 0; c < 32; c++)
+	{
+		ans += d[c];
+		if (ans || c == 31)
+		{
+			char z = '0' + d[c];
 
-    while (number > 0)
-    {
-        outputBuffer[x--] = (number % 2) + '0';
-        number /= 2;
-    }
-
-   x++;
-
-    return writeNumber(0, x, outputBuffer, activeFlags, fieldWidth, precision, dataSize);
+			num++;
+		}
+	}
+	return (num);
 }
